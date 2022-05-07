b9fb0852494a3ce71bf095d67a2f3d0a003546f8
AfendyWan <afendywan1999@gmail.com>
2022-05-07 13:14:06 +0800

    Add sign in and sign up UI template

---
+++ lib/screens/Signup/signup_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:rezeki_bundle_mobile/screens/Signup/components/body.dart';
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
---
+++ lib/screens/Welcome/welcome_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:rezeki_bundle_mobile/screens/Welcome/components/body.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
