5241ce728ce1e8a35147d3708d347c504fe15a14
AfendyWan <afendywan1999@gmail.com>
2022-05-11 11:50:21 +0800

    Complete dependepent dropdown for states and cities in register user interface

--- lib/api/setting_api.dart
+++ lib/api/setting_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:rezeki_bundle_mobile/model/city.dart';
 import 'package:rezeki_bundle_mobile/model/state.dart';
 import 'package:rezeki_bundle_mobile/model/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -12,7 +13,7 @@ import '../screens/Dashboard/dashboard.dart';
 
 getAllStates() async {
   var url = "http://192.168.0.157:8000/api/settings/showAllStates";
-  
+
   var response = await http.get(Uri.parse(url), headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
@@ -22,9 +23,30 @@ getAllStates() async {
   if (response.statusCode == 200) {
     var jsonResponse = jsonDecode(response.body);
 
-    List<Negeri> negeri = List<Negeri>.from(
-        jsonResponse.map((model) => Negeri.fromJson(model)));
+    List<Negeri> negeri =
+        List<Negeri>.from(jsonResponse.map((model) => Negeri.fromJson(model)));
     return negeri;
+  } else {
+    print("Failed");
+  }
+}
+
+getAllCities() async {
+  var url = "http://192.168.0.157:8000/api/settings/showAllCities";
+
+  var response = await http.get(Uri.parse(url), headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  });
+
+  //get api result
+  if (response.statusCode == 200) {
+    var jsonResponse = jsonDecode(response.body);
+
+    List<City> city =
+        List<City>.from(jsonResponse.map((model) => City.fromJson(model)));
+    
+    return city;
   } else {
     print("Failed");
   }
--- lib/screens/Signup/components/body.dart
+++ lib/screens/Signup/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rezeki_bundle_mobile/api/setting_api.dart';
 import 'package:rezeki_bundle_mobile/components/text_field_container.dart';
 import 'package:rezeki_bundle_mobile/constants.dart';
+import 'package:rezeki_bundle_mobile/model/city.dart';
 import 'package:rezeki_bundle_mobile/model/state.dart';
 import 'package:rezeki_bundle_mobile/screens/Login/login_screen.dart';
 import 'package:rezeki_bundle_mobile/screens/Signup/components/background.dart';
@@ -48,21 +49,38 @@ class _BodyState extends State<Body> {
 
   //for states dropdown variables
   Negeri? _selectedStates;
+  City? _selectedCities;
   var states;
+  var cities;
   var check;
   List<Negeri> _statesList = [];
-
+  List<City>? _cityList = [];
+  List<City>? _onChangesCityList = [];
   getData() async {
     _statesList.clear();
-    states = await getAllStates();
 
+    //take state list from api GET method call
+    states = await getAllStates();
+    cities = await getAllCities();
     for (var data in states) {
+      //transfer states list from GET method call to a new one
       _statesList.add(Negeri(id: data.id, statesName: data.statesName));
     }
-    if (_statesList.isNotEmpty) {
-      _selectedStates = _statesList[0];
+
+    for (var data in cities) {
+      //transfer city list from GET method call to a new one
+      _cityList!.add(City(
+          id: data.id, citiesName: data.citiesName, statesId: data.statesId));
+      _onChangesCityList!.add(City(
+          id: data.id, citiesName: data.citiesName, statesId: data.statesId));
     }
-  
+
+    if (_statesList.isNotEmpty) {
+      //set defaul value in dropdown
+      //_selectedStates = _statesList[0];
+      _selectedCities = null;
+    }
+
     check = true;
     return check;
   }
@@ -297,10 +315,26 @@ class _BodyState extends State<Body> {
                                 value: _selectedStates,
                                 items: _statesList.map((Negeri negeri) {
                                   return DropdownMenuItem(
-                                      value: negeri, child: Text(negeri.statesName!));
+                                      value: negeri,
+                                      child: Text(negeri.statesName!));
                                 }).toList(),
-                                onChanged: (value) {
-                                  setState(() {});
+                                onChanged: (value) async {
+                                  setState(() {
+                                    _onChangesCityList?.clear();
+                                   
+                                    if (_cityList != null) {
+                                      for (var data in _cityList!) {
+                                        if (data.statesId == value?.id) {
+                                          _onChangesCityList?.add(City(
+                                              id: data.id,
+                                              citiesName: data.citiesName,
+                                              statesId: data.statesId));
+                                        }
+                                      }
+                                       _selectedCities = _onChangesCityList![0];
+
+                                    }
+                                  });
                                 },
                                 validator: (value) {
                                   if (value == null) {
@@ -311,7 +345,8 @@ class _BodyState extends State<Body> {
                               ),
                             ),
                             TextFieldContainer(
-                              child: DropdownButtonFormField<String>(
+                              child: DropdownButtonFormField<City>(
+                                isExpanded: true,
                                 decoration: const InputDecoration(
                                     icon: Icon(
                                       Icons.home_work_rounded,
@@ -320,13 +355,14 @@ class _BodyState extends State<Body> {
                                     enabledBorder: InputBorder.none,
                                     contentPadding: EdgeInsets.symmetric(
                                         vertical: 4, horizontal: 8)),
-                                hint: const Text(
+                                hint: Text(
                                   "Please select your city",
                                 ),
-                                value: selectedValue,
-                                items: items.map((String items) {
+                                value: _selectedCities,
+                                items: _onChangesCityList?.map((City city) {
                                   return DropdownMenuItem(
-                                      value: items, child: Text(items));
+                                      value: city,
+                                      child: Text(city.citiesName!));
                                 }).toList(),
                                 onChanged: (value) {
                                   setState(() {});
